65f2f86e8b0ea2fa1a05bff19079372dcf42c5a7
Liming Xie <liming.xie@gmail.com>
2016-02-20 23:26:17 +0800

    Add README

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 var mudinfo = [
   {
-    name: '东方故事II (utf8)',
+    name: '东方故事II',
     desc: '',
     proj_url: 'https://github.com/mudchina/es2-mudlib-utf8',
+    charset: 'UTF8',
     game_url: '',
   },
   {
-    name: '侠客行100 (gbk)',
+    name: '侠客行100',
     desc: '',
     proj_url: 'https://github.com/mudchina/xkx100',
+    charset: 'GBK',
     game_url: '',
   },
   {
-    name: '梦幻西游2002版 (gbk)',
+    name: '侠客行2001',
+    desc: '',
+    proj_url: 'https://github.com/mudchina/xkx2001',
+    charset: 'GBK',
+    game_url: '',
+  },
+  {
+    name: '梦幻西游2002版',
     desc: '',
     proj_url: 'https://github.com/mudchina/mhxy2002',
+    charset: 'GBK',
     game_url: '',
   },
 ];
 
 $(document).ready(function(){
   var mudlist = $('<table>').appendTo('div#mudlist');
-  mudlist.append('<tr><th>游戏名称</th><th>简介</th><th>项目／源代码</th><th>游戏网址</th></tr>');
+  mudlist.append('<tr><th></th><th>游戏名称</th><th>简介</th><th>字符集/编码</th><th>项目／源代码</th><th>DEMO网址</th></tr>');
 
   var n = mudinfo.length;
   for(var i=0; i<n; i++) {
     var mud = mudinfo[i];
-    mudlist.append('<tr><td>' + mud.name + '</td><td>' + mud.desc + '</td><td>' + mud.proj_url + '</td><td>' + mud.game_url + '</td></tr>');
+    mudlist.append('<tr><td>' + (i+1) + '</td><td>' + mud.name + '</td><td>' + mud.desc + '</td><td>' + mud.charset + '</td><td><a href="' + mud.proj_url + '">' + mud.proj_url + '</a></td><td>' + mud.game_url + '</td></tr>');
   }
 });
